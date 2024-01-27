9171b86777d858bc30d1348294a3eb367e9cad04
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2024-01-27 14:31:29 -0300

    Update bodyContainer.tsx

--- front-end-code-challenge-smartfit/src/components/bodyContanier/bodyContainer.tsx
+++ front-end-code-challenge-smartfit/src/components/bodyContanier/bodyContainer.tsx
@@ -1,9 +1,11 @@
 import { Anuncio } from "../anuncio/anuncio";
+import { Horario } from "../horarioContainer/horarios";
 import styles from "./bodyContainer.module.scss";
 export function BodyContainer() {
   return (
     <div className={styles.Container}>
       <Anuncio />
+      <Horario />
     </div>
   );
 }
