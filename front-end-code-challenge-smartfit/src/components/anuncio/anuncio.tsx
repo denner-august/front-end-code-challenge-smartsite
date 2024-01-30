1e5845354ba827fb84a7b299a7b955b48e7f0b5f
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2024-01-30 20:26:58 -0300

    Update anuncio.tsx

--- front-end-code-challenge-smartfit/src/components/anuncio/anuncio.tsx
+++ front-end-code-challenge-smartfit/src/components/anuncio/anuncio.tsx
@@ -10,8 +10,8 @@ export function Anuncio() {
 
       <p>
         O horário de funcionamento das nossas unidades está seguindo os decretos
-        de cada município. Por isso,confira aqui se a sua unidade está aberta e
-        as medidas de segurança que estamos seguindo
+        de cada município. Por isso, confira aqui se a sua unidade está aberta e
+        as medidas de segurança que estamos seguindo.
       </p>
     </main>
   );
