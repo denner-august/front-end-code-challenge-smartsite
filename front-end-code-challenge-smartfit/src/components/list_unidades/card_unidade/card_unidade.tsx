3612927112e0950e253774c955ad3e426f61825f
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2024-02-01 20:29:38 -0300

    Update card_unidade.tsx

--- front-end-code-challenge-smartfit/src/components/list_unidades/card_unidade/card_unidade.tsx
+++ front-end-code-challenge-smartfit/src/components/list_unidades/card_unidade/card_unidade.tsx
@@ -1,15 +1,32 @@
+"use client";
 import Image from "next/image";
 import styles from "./card_unidade.module.scss";
 import { legenda } from "@/components/legenda";
+import { useEffect, useState } from "react";
+
 export function CardUnidade() {
+  async function t() {
+    const res = await fetch(
+      "https://test-frontend-developer.s3.amazonaws.com/data/locations.json"
+    ).then((resonse) => console.log(resonse));
+  }
+
+  useEffect(() => {
+    t();
+  }, []);
+
+  const [cor, setcor] = useState(false);
+
   return (
     <div className={styles.Container}>
-      <span>Aberto</span>
+      <span style={{ color: cor === true ? "#2fbd04" : "#ff0000" }}>
+        fechado
+      </span>
 
       <p>Vicente Linhares</p>
       <span>Dom Severino, 1733 Fátima Teresina, PI</span>
 
-      <ul>
+      <ul className={styles.division}>
         <Image
           src={legenda.Mask.required.image}
           alt={legenda.Mask.required.title}
@@ -36,7 +53,11 @@ export function CardUnidade() {
         />
       </ul>
 
-      <ul>
+      <ul className={styles.semana}>
+        <li>
+          <p>weekdays</p>
+          <span>hour</span>
+        </li>
         <li>
           <p>weekdays</p>
           <span>hour</span>
